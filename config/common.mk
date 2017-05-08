2e484f946e2e0c0ed83abb24ff4556f40edf39b0
RakeshBatra <raakesh.batra@rediffmail.com>
2017-05-08 08:00:36 +0000

    Add our bootanimation
    Change-Id: Ifec8b0679a23c6286c46004f4829ca9c34ae6bbc
    Signed-off-by: RakeshBatra <raakesh.batra@rediffmail.com>

--- config/common.mk
+++ config/common.mk
@@ -1,5 +1,9 @@
 PRODUCT_BRAND ?= ColtOS
 
+# BootAnimation
+PRODUCT_COPY_FILES += \
+    vendor/cm/prebuilt/common/bootanimation/bootanimation.zip:system/media/bootanimation.zip
+
 PRODUCT_BUILD_PROP_OVERRIDES += BUILD_UTC_DATE=0
 
 ifeq ($(PRODUCT_GMS_CLIENTID_BASE),)
@@ -88,10 +92,6 @@ ifneq ($(TARGET_DISABLE_CMSDK), true)
 # CMSDK
 include vendor/cm/config/cmsdk_common.mk
 endif
-
-# Bootanimation
-PRODUCT_PACKAGES += \
-    bootanimation.zip
 
 # Required CM packages
 PRODUCT_PACKAGES += \
